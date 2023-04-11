018e5c4483dbb72f8dc8525c04438d937a586007
ohrtann19 <anna_ohrt@brown.edu>
2023-04-10 21:17:49 -0400

    remove title bc feedback

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Computers Performing</h1>
+      {/* <h1>Computers Performing</h1> */}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
